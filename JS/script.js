af47c9bdff1c750f6c55cbba3a9d19efcde1bf11
Yaroslav0309 <Yaroslav9d@gmail.com>
2017-12-22 15:47:08 +0200

    olymp

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
-  var carousels = document.querySelectorAll('#carousels .carousel');
+  var carousels = document.querySelectorAll('#started .carousel');
   var currentCarousel = 0;
-  var carouselInterval = setInterval(nextCarousel,4000); /* Интервал между картинками */
+  var carouselInterval = setInterval(nextCarousel,3000); /* Интервал между картинками */
 
   function nextCarousel(){
-	carousels[currentCarousel].className = 'carousel';
-	currentCarousel = (currentCarousel+1)%carousels.length;
-	carousels[currentCarousel].className = 'carousel demonstration';
-} 
+  carousels[currentCarousel].className = 'carousel';
+  currentCarousel = (currentCarousel+1)%carousels.length;
+  carousels[currentCarousel].className = 'carousel demonstration';
+}
